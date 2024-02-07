8008eb99a08eaa9272c28a010e2d6fd69afa979b
alexiatanasie <alexia.tanasie@yahoo.com>
2024-02-07 13:23:37 +0200

    drop tables

---
+++ drop tables.sql
@@ -0,0 +1,13 @@
+
+--drop tables
+drop table "REGIONS" cascade constraints;
+drop table "COUNTRIES" cascade constraints;
+drop table "CUSTOMERS" cascade constraints;
+drop table "DEPARTMENTS" cascade constraints;
+drop table "EMPLOYEES" cascade constraints;
+drop table "JOB_HISTORY" cascade constraints;
+drop table "JOBS" cascade constraints;
+drop table "LOCATIONS" cascade constraints;
+drop table "ORDER_ITEMS"cascade constraints;
+drop table "ORDERS" cascade constraints;
+drop table "PRODUCT_INFORMATION" cascade constraints;
